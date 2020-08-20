14756cff865fb5ac51f99ebcee5036ab8144fe29
SilvenLEAF <SilvenLEAF@gmail.com>
2020-08-20 14:30:59 +0530

    renamed middlewares to routes on app.js backend

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 
 
 /* --------------------------------------
-.               middlewares
+.               routes
 -------------------------------------- */
 app.use(bodyParser.json());
 
